5e791e9eb6a4a7c38475b348f9d57edeb84900c2
Rahim10020 <rahialighi@gmail.com>
2025-03-09 16:01:46 +0000

    Finition du searchBar

--- app/src/main/java/tg/rahimali/filmsapp/activity/MainActivity.kt
+++ app/src/main/java/tg/rahimali/filmsapp/activity/MainActivity.kt
@@ -7,27 +7,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import tg.rahimali.filmsapp.BottomNavigationBar
 import tg.rahimali.filmsapp.R
+import tg.rahimali.filmsapp.SearchBar
 import tg.rahimali.filmsapp.data.FilmItemModel
 import tg.rahimali.filmsapp.ui.theme.FilmsAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -82,6 +90,28 @@ fun MainScreen(onItemClick: (FilmItemModel) -> Unit = {}) {
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.matchParentSize()
             )
+            MainContent(onItemClick)
         }
+    }
+}
+
+@Composable
+fun MainContent(onItemClick: (FilmItemModel) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(top=60.dp, bottom = 100.dp)
+    ) {
+        Text(
+            text = "What would you want to watch ?",
+            fontSize = 19.sp,
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding( start = 19.dp, bottom = 16.dp)
+                .fillMaxWidth()
+        )
+        SearchBar("Search Movies...")
     }
 }
